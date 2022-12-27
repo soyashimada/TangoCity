a4f3d7ebf02a102b4716b636c71e95b8a221b6b7
Soya Shimada <soya.shimada@hva.nl>
2022-12-27 01:05:37 +0100

    add editFragment and connected tablayout

--- app/src/main/java/nl/hva/tangocity/ui/review/ReviewFragment.kt
+++ app/src/main/java/nl/hva/tangocity/ui/review/ReviewFragment.kt
@@ -4,15 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toolbar
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.tabs.TabLayout
 import nl.hva.tangocity.R
 import nl.hva.tangocity.databinding.FragmentReviewBinding
 
@@ -31,14 +28,30 @@ class ReviewFragment : Fragment() {
         _binding = FragmentReviewBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        val navController = findNavController()
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolBar.setupWithNavController(navController, appBarConfiguration)
+
         return root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val navController = findNavController()
-        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        val nestedNavHostFragment = childFragmentManager.findFragmentById(R.id.nav_host_fragment_fragment_review) as? NavHostFragment
+        val navController = nestedNavHostFragment?.navController
+        binding.tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when(tab?.position)
+                {
+                    0 -> navController?.navigate(R.id.navigation_study)
+                    2 -> navController?.navigate(R.id.navigation_edit)
+                }
+            }
 
-        binding.toolBar.setupWithNavController(navController, appBarConfiguration)
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+            }
+            override fun onTabReselected(tab: TabLayout.Tab) {
+            }
+        })
     }
 
     override fun onDestroyView() {
