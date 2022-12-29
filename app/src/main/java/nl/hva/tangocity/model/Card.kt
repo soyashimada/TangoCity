19c6eda0614bd6d2d5b032c2504867a097c75be7
Soya Shimada <soya.shimada@hva.nl>
2022-12-29 06:36:45 +0100

    fix create dialog

--- app/src/main/java/nl/hva/tangocity/model/Card.kt
+++ app/src/main/java/nl/hva/tangocity/model/Card.kt
@@ -5,8 +5,8 @@ data class Card (
     val question: String,
     val answer: String,
     val repetition: Int = 0,
-    val easiness_factor: Float,
+    val easinessFactor: Float,
     val interval: Int,
-    val next_date: String,
-    val last_result: Int
+    val nextDate: String,
+    val lastResult: Int
 )
--- app/src/main/java/nl/hva/tangocity/ui/create/CreateDialogFragment.kt
+++ app/src/main/java/nl/hva/tangocity/ui/create/CreateDialogFragment.kt
@@ -8,16 +8,16 @@ import android.view.ViewGroup
 import android.view.Window
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import nl.hva.tangocity.R
 import nl.hva.tangocity.databinding.CreateDialogBinding
+import nl.hva.tangocity.viewModel.DeckViewModel
 
 class CreateDialogFragment : DialogFragment() {
     private var _binding: CreateDialogBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private val viewModel: DeckViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,12 +26,20 @@ class CreateDialogFragment : DialogFragment() {
     ): View {
         _binding = CreateDialogBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        val transaction = parentFragmentManager.beginTransaction()
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE)
 
         binding.cancelButton.setOnClickListener{
             findNavController().navigate(R.id.navigation_home)
 
-            val transaction = parentFragmentManager.beginTransaction()
-            transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE)
+            transaction.hide(this).commit()
+        }
+
+        binding.doneButton.setOnClickListener{
+            viewModel.createDeckAndCard(binding.deckNameInput.text.toString(),
+                binding.questionInput.text.toString(), binding.answerInput.text.toString())
+
+            findNavController().navigate(R.id.navigation_home)
             transaction.hide(this).commit()
         }
 
@@ -40,10 +48,6 @@ class CreateDialogFragment : DialogFragment() {
 
     /** The system calls this only when creating the layout in a dialog. */
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        // The only reason you might override this method when using onCreateView() is
-        // to modify any dialog characteristics. For example, the dialog includes a
-        // title by default, but your custom layout might not need it. So here you can
-        // remove the dialog title, but you must call the superclass to get the Dialog.
         val dialog = super.onCreateDialog(savedInstanceState)
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         return dialog
