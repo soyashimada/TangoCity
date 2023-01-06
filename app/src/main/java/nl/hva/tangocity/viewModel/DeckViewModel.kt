b9a0af428fe7375df26963250739dd32df3ed5d1
Soya Shimada <soya.shimada@hva.nl>
2023-01-06 03:00:09 +0100

    add createCard function in deckViewmodel

--- app/src/main/java/nl/hva/tangocity/viewModel/DeckViewModel.kt
+++ app/src/main/java/nl/hva/tangocity/viewModel/DeckViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
+import nl.hva.tangocity.model.Card
 import nl.hva.tangocity.model.Deck
 import nl.hva.tangocity.repository.DeckRepository
 
@@ -35,12 +36,40 @@ class DeckViewModel(application: Application) : AndroidViewModel(application)  {
         }
     }
 
-    fun createDeckAndCard(deckName: String, question: String, answer: String) {
+    fun createDeckAndCard(deckName: String, question: String, answer: String, callback: () -> Unit) {
         viewModelScope.launch {
             try {
                 deckRepository.createDeckAndCard(deckName, question, answer)
+                callback()
             } catch (ex: DeckRepository.SaveError) {
                 val errorMsg = "Something went wrong while saving the deck"
+                Log.e(TAG, ex.message ?: errorMsg)
+                _errorText.value = errorMsg
+            }
+        }
+    }
+
+    fun createCard(deckPosition: Int, question: String, answer: String, cardPosition: Int? = null, callback: () -> Unit) {
+        viewModelScope.launch {
+            try {
+                val deck: Deck? = decks.value?.get(deckPosition)
+                val deckId = deck?.id ?: 1
+                val cardId = if (cardPosition != null)
+                    deck?.cards?.get(cardPosition)?.id ?: 1 else null
+
+                val newCardId = deckRepository.createCard(deckId, question, answer, cardId)
+
+                deckRepository.decks.value?.let {
+                    if (cardPosition != null && cardId != null){
+                        it[deckPosition].cards[cardPosition].question = question
+                        it[deckPosition].cards[cardPosition].answer = answer
+                    }else {
+                        it[deckPosition].cards.add(Card(newCardId, question, answer, 0,0.0,0,"0",0))
+                    }
+                }
+                callback()
+            } catch (ex: DeckRepository.SaveError) {
+                val errorMsg = "Something went wrong while saving the card"
                 Log.e(TAG, ex.message ?: errorMsg)
                 _errorText.value = errorMsg
             }
