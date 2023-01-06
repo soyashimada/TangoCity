7cfe06bd07f341f2894d74914c9fb0fc3bd9fb32
Soya Shimada <soya.shimada@hva.nl>
2023-01-06 03:01:31 +0100

    edit ReviewFragment; use ReviewViewModel and adapt tabBar

--- app/src/main/java/nl/hva/tangocity/ui/home/review/ReviewFragment.kt
+++ app/src/main/java/nl/hva/tangocity/ui/home/review/ReviewFragment.kt
@@ -1,10 +1,12 @@
-package nl.hva.tangocity.ui.review
+package nl.hva.tangocity.ui.home.review
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -12,13 +14,15 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.tabs.TabLayout
 import nl.hva.tangocity.R
 import nl.hva.tangocity.databinding.FragmentReviewBinding
+import nl.hva.tangocity.viewModel.DeckViewModel
+import nl.hva.tangocity.viewModel.ReviewViewModel
 
 class ReviewFragment : Fragment() {
     private var _binding: FragmentReviewBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private val deckViewModel: DeckViewModel by activityViewModels()
+    private val viewModel: ReviewViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,18 +36,38 @@ class ReviewFragment : Fragment() {
         val appBarConfiguration = AppBarConfiguration(navController.graph)
         binding.toolBar.setupWithNavController(navController, appBarConfiguration)
 
+        setFragmentResultListener("selectedDeck"){ requestKey, bundle ->
+            val position = bundle.getInt("position")
+            if (viewModel.selectedDeckPosition.value != position){
+                viewModel.resetSelectedTab()
+                viewModel.updateSelectedDeckPosition(position)
+            }
+            val deck = deckViewModel.decks.value?.get(viewModel.selectedDeckPosition.value!!)
+            binding.toolBar.title = deck?.name ?: "undefined"
+        }
+
         return root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val nestedNavHostFragment = childFragmentManager.findFragmentById(R.id.nav_host_fragment_fragment_review) as? NavHostFragment
         val navController = nestedNavHostFragment?.navController
+
+        viewModel.selectedTab.observe(viewLifecycleOwner){
+            binding.tabLayout.selectTab(binding.tabLayout.getTabAt(it))
+        }
+
         binding.tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                when(tab?.position)
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                viewModel.updateSelectedTab(tab.position)
+                when(tab.position)
                 {
-                    0 -> navController?.navigate(R.id.navigation_study)
-                    2 -> navController?.navigate(R.id.navigation_edit)
+                    0 -> {
+                        navController?.navigate(R.id.navigation_study)
+                    }
+                    2 -> {
+                        navController?.navigate(R.id.navigation_edit)
+                    }
                 }
             }
 
