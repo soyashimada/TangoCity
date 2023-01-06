2d2a3a03ff04706e071cbc6fcd90f2fd392db6dd
Soya Shimada <soya.shimada@hva.nl>
2023-01-06 03:01:53 +0100

    fix StudyFragment

--- app/src/main/java/nl/hva/tangocity/ui/home/review/study/StudyFragment.kt
+++ app/src/main/java/nl/hva/tangocity/ui/home/review/study/StudyFragment.kt
@@ -1,18 +1,21 @@
-package nl.hva.tangocity.ui.review.study
+package nl.hva.tangocity.ui.home.review.study
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import nl.hva.tangocity.databinding.FragmentStudyBinding
+import nl.hva.tangocity.viewModel.DeckViewModel
+import nl.hva.tangocity.viewModel.ReviewViewModel
 
 class StudyFragment : Fragment() {
     private var _binding: FragmentStudyBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private val viewModel: ReviewViewModel by activityViewModels()
+    private val deckViewModel: DeckViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,6 +24,11 @@ class StudyFragment : Fragment() {
     ): View {
         _binding = FragmentStudyBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        viewModel.selectedDeckPosition.observe(viewLifecycleOwner) { position ->
+            val cards = deckViewModel.decks.value?.let { it[position].cards }
+            binding.questionContent.text = cards?.get(0)?.question ?: "undefined"
+        }
 
         return root
     }
