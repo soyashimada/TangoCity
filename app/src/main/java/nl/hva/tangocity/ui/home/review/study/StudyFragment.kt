1cafa8bf9cc723eed06f90bfdd28643a6ca87f49
Soya Shimada <soya.shimada@hva.nl>
2023-01-07 07:28:05 +0100

    edit study fragment

--- app/src/main/java/nl/hva/tangocity/ui/home/review/study/StudyFragment.kt
+++ app/src/main/java/nl/hva/tangocity/ui/home/review/study/StudyFragment.kt
@@ -1,18 +1,18 @@
 package nl.hva.tangocity.ui.home.review.study
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import nl.hva.tangocity.databinding.FragmentStudyBinding
 import nl.hva.tangocity.model.Card
 import nl.hva.tangocity.viewModel.DeckViewModel
 import nl.hva.tangocity.viewModel.ReviewViewModel
+import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.math.round
 
 class StudyFragment : Fragment() {
     private var _binding: FragmentStudyBinding? = null
@@ -38,17 +38,22 @@ class StudyFragment : Fragment() {
         viewModel.currentCardSide.observe(viewLifecycleOwner) { side ->
             viewModel.studyPosition.observe(viewLifecycleOwner) { position ->
                 studyPosition = position
-                Log.i("test", studyPosition.toString())
-
                 if (studyCards.size <= (position ?: 0)) {
-                    binding.questionContent.isVisible = false
+                    // in the case that cards aren't left
+                    binding.finishImage.visibility = View.VISIBLE
+                    binding.finishText.visibility = View.VISIBLE
+                    binding.cardLayout.visibility = View.INVISIBLE
+                    changeButtonsVisible(View.INVISIBLE)
+
                 }else {
                     binding.questionContent.text = if (side) {
                         //answer
+                        changeButtonsVisible(View.VISIBLE)
                         studyCards[studyPosition].answer
 
                     } else {
                         //question
+                        changeButtonsVisible(View.INVISIBLE)
                         studyCards[studyPosition].question
                     }
                 }
@@ -60,7 +65,23 @@ class StudyFragment : Fragment() {
         }
 
         binding.buttonAnswer1.setOnClickListener{
-            viewModel.nextCard()
+            calculateCardResult(1, viewModel.selectedDeckPosition.value!!, studyCards[studyPosition])
+        }
+
+        binding.buttonAnswer2.setOnClickListener{
+            calculateCardResult(2, viewModel.selectedDeckPosition.value!!, studyCards[studyPosition])
+        }
+
+        binding.buttonAnswer3.setOnClickListener{
+            calculateCardResult(3, viewModel.selectedDeckPosition.value!!, studyCards[studyPosition])
+        }
+
+        binding.buttonAnswer4.setOnClickListener{
+            calculateCardResult(4, viewModel.selectedDeckPosition.value!!, studyCards[studyPosition])
+        }
+
+        binding.buttonAnswer5.setOnClickListener{
+            calculateCardResult(5, viewModel.selectedDeckPosition.value!!, studyCards[studyPosition])
         }
 
         return root
@@ -69,5 +90,45 @@ class StudyFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun calculateCardResult(result: Int, deckPosition: Int, card: Card) {
+        var repetition = card.repetition
+        var ease: Double =  card.easinessFactor
+        var interval = card.interval
+
+        if (result >= 3) {
+            //correct response
+            interval = when (repetition) {
+                0 -> 1
+                1 -> 6
+                else -> round(ease * interval).toInt()
+            }
+            repetition += 1
+        } else {
+            //incorrect response
+            repetition = 0
+            interval = 1
+        }
+        ease += (0.1 - (5 - result) * (0.08 + (5 - result) * 0.02))
+        if (ease < 1.3) {
+            ease = 1.3
+        }
+
+        card.repetition = repetition
+        card.easinessFactor = ease
+        card.interval = interval
+        card.nextDate.add(Calendar.DATE, interval)
+        card.lastResult = result
+
+        deckViewModel.editCard(deckPosition, card) { viewModel.nextCard() }
+    }
+
+    private fun changeButtonsVisible(mode: Int) {
+        binding.answer1.visibility = mode
+        binding.answer2.visibility = mode
+        binding.answer3.visibility = mode
+        binding.answer4.visibility = mode
+        binding.answer5.visibility = mode
     }
 }
