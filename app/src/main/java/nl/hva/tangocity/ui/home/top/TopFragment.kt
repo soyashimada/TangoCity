b8b1638577ca1a16406541c30431f27f989682eb
Soya Shimada <soya.shimada@hva.nl>
2023-01-07 23:59:54 +0100

    edit Calender in top page; show review

--- app/src/main/java/nl/hva/tangocity/ui/home/top/TopFragment.kt
+++ app/src/main/java/nl/hva/tangocity/ui/home/top/TopFragment.kt
@@ -1,11 +1,14 @@
 package nl.hva.tangocity.ui.home.top
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.os.bundleOf
 import androidx.core.view.children
 import androidx.fragment.app.Fragment
@@ -23,8 +26,10 @@ import nl.hva.tangocity.databinding.FragmentTopBinding
 import nl.hva.tangocity.ui.home.top.calendar.DayViewContainer
 import nl.hva.tangocity.viewModel.DeckViewModel
 import java.time.YearMonth
+import java.time.format.DateTimeFormatter
 import java.time.format.TextStyle
 import java.util.*
+import kotlin.math.roundToInt
 
 
 class TopFragment : Fragment() {
@@ -49,10 +54,11 @@ class TopFragment : Fragment() {
         (activity as AppCompatActivity).setSupportActionBar(binding.toolBar)
 
         val calendarView = binding.reviewCalendar
-        val currentMonth = YearMonth.now()
-        val startMonth = currentMonth.minusMonths(100)  // Adjust as needed
-        val endMonth = currentMonth.plusMonths(100)  // Adjust as needed
-        val daysOfWeek = daysOfWeek()// Available from the library
+        val thisMonth = YearMonth.now()
+        var currentMonth = thisMonth
+        val startMonth = thisMonth.minusMonths(100)
+        val endMonth = thisMonth.plusMonths(100)
+        val daysOfWeek = daysOfWeek()
 
         val titlesContainer = root.findViewById<ViewGroup>(R.id.titlesContainer)
         titlesContainer.children
@@ -64,7 +70,10 @@ class TopFragment : Fragment() {
             }
 
         calendarView.setup(startMonth, endMonth, daysOfWeek.first())
-        calendarView.scrollToMonth(currentMonth)
+        calendarView.scrollToMonth(thisMonth)
+        calendarView.monthScrollListener = {
+            binding.monthYearTxt.text = it.yearMonth.format(DateTimeFormatter.ofPattern("LLL yyyy"))
+        }
 
         calendarView.dayBinder = object : MonthDayBinder<DayViewContainer> {
             // Called only when a new container is needed.
@@ -72,8 +81,48 @@ class TopFragment : Fragment() {
 
             // Called every time we need to reuse a container.
             override fun bind(container: DayViewContainer, data: CalendarDay) {
+                val thisDay = Calendar.getInstance()
+                thisDay.set(data.date.year, data.date.monthValue-1, data.date.dayOfMonth)
+                thisDay.set(Calendar.MILLISECOND, 0)
+                thisDay.set(Calendar.SECOND, 0)
+                thisDay.set(Calendar.MINUTE, 0)
+                thisDay.set(Calendar.HOUR_OF_DAY, 0)
+
+                val nextDay = thisDay.clone() as Calendar
+                nextDay.add(Calendar.DAY_OF_MONTH, 1)
+
                 container.textView.text = data.date.dayOfMonth.toString()
+                viewModel.reviews.observe(viewLifecycleOwner){
+                    // the list of Review at this day
+                    val reviews = it.filter {review -> review.date >= thisDay && review.date < nextDay }
+
+                    // if user study at this day
+                    if (reviews.isNotEmpty()) {
+                        // the target how many user want to memorize
+                        val targetNum = 100.0
+                        // the number to do this day / the target
+                        var percent = ((reviews.size/targetNum)*60.0).toInt()
+                        percent = percent.let {p -> if (p < 20) 20 else if (p > 60) 60 else p }
+
+                        //update layout
+                        container.textView.setTextColor(Color.parseColor(getColor(R.color.normal_text)))
+                        val circle: ImageView = container.view.findViewById(R.id.calendarDayCircle)
+                        val colorCode = getColor(R.color.normal_icon, percent)
+                        circle.setColorFilter(Color.parseColor(colorCode))
+                        circle.visibility = View.VISIBLE
+                    }
+                }
             }
+        }
+
+        binding.monthYearTxt.text =  thisMonth.format(DateTimeFormatter.ofPattern("LLL yyyy"))
+        binding.nextMonthImg.setOnClickListener{
+            currentMonth = currentMonth.plusMonths(1)
+            calendarView.scrollToMonth(currentMonth)
+        }
+        binding.previousMonthImg.setOnClickListener{
+            currentMonth = currentMonth.minusMonths(1)
+            calendarView.scrollToMonth(currentMonth)
         }
 
         navController = findNavController()
@@ -103,5 +152,18 @@ class TopFragment : Fragment() {
         navController.navigate(
             R.id.action_TopFragment_to_ReviewFragment
         )
+    }
+
+    private fun getColor(color: Int, percent: Int = 100): String{
+        return ("#" + percentToHex(percent) + Integer.toHexString(
+            ContextCompat.getColor(
+                requireContext(), color
+            ) and 0x00ffffff).uppercase(Locale.getDefault())
+        )
+    }
+
+    private fun percentToHex(percent: Int): String{
+        return (255.0 * (percent / 100.0)).roundToInt().toString( 16 )
+            .uppercase(Locale.getDefault()).padStart(2, '0')
     }
 }
